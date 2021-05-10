b7e816cd4cb602ceb79053d58d4d3f92900a252a
guanwenbo <guanwenbo@inke.cn>
2021-05-10 16:11:25 +0800

    feat: 判断EOF

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -9,16 +10,15 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-	})
+	r.GET("/ping", pingHandle)
 	r.Run(":11001") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func pingHandle(c *gin.Context) {
 	body := make(map[string]interface{})
 
-	err := c.Bind(&body)
-	if err != nil {
+	err := c.BindJSON(&body)
+	if err != nil && err != io.EOF {
 		log.Println("ping bind body failed. err=%v", err)
 		c.AbortWithError(400, err)
 		return
